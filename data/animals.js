2e39312ce6b4bd26c7d2d69d92b06e674efe5fb4
Cristian Montes <cmontesmujica@gmail.com>
2021-08-19 09:47:33 -0700

    getting names from table

--- data/animals.js
+++ data/animals.js
@@ -1,16 +1,16 @@
-module.exports = ['bessie', 'jumpy', 'spot'];
+module.exports = [
 
 
-//   {
-//     name: 'bessie',
-//     cool_factor: 3,
-//   },
-//   {
-//     name: 'jumpy',
-//     cool_factor: 4,
-//   },
-//   {
-//     name: 'spot',
-//     cool_factor: 10,
-//   }
-// ];
+  {
+    name: 'bessie',
+    cool_factor: 3,
+  },
+  {
+    name: 'jumpy',
+    cool_factor: 4,
+  },
+  {
+    name: 'spot',
+    cool_factor: 10,
+  }
+];
